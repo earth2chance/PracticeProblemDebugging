226c9dd9f48cc7dac98ff8d4a417f1652dec1aae
earth2chance <cdaily.spam@gmail.com>
2017-04-03 10:13:02 -0500

    2.0 assignment
    done 4/3/17

---
+++ DebuggingThing/DebuggingThing/DebuggingAssign.cpp
@@ -0,0 +1,20 @@
+//SwatTheBugs16.cpp
+//Created/revised by Chance Daily on 4-3-2017
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//declare variable
+	double temp = 35.3;
+
+	//increase variable's value by 1.5
+	temp = temp + 1.5;
+
+		//display variable's value
+		std::cout << "After adding 1.5, the temp variable now contains " << temp << "." << endl;
+
+	system("pause");
+	return 0;
+}	//end of main function
